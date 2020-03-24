902a5e0b28a53d9957c87aed6b6cffd08e0b5032
developerXed <edi.dimitrov12@gmail.com>
2020-03-25 00:26:47 +0200

    upload project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AngularMaterialtableComponent } from './helpers/angular-material-table/angular-material-table.component';
+import { TableComponent } from './table/table.component';
+import { TableTwoComponent } from './table-two/table-two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'table', component:AngularMaterialtableComponent},
+  {path:'table-1', component:TableComponent},
+  {path:'table-2', component:TableTwoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
